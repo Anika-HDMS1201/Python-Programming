f48415ae7df270d8585e611991430489efde502c
Anika-HDMS1201 <anikajana79@gmail.com>
2026-05-15 16:43:13 +0530

    Homework of 2 days

---
+++ Training/Programs/Home_Work/Day 3/HW_Day3.py
@@ -0,0 +1,15 @@
+#Task 1
+txt = "Hello World"
+print(txt[2:5])
+
+print(txt.upper())
+
+poo="Python"
+boo=f"I love {poo}"
+print(boo)
+
+#Task 2
+print(10>9)
+print(10==9)
+print(bool("Hello"))
+print(bool(0))
---
+++ Training/Programs/Home_Work/Day 4/HW_Day4.py
@@ -0,0 +1,14 @@
+#Task 
+
+a=15
+b=4
+print(a%b)
+print(a//b)
+
+a+=10
+print(a)
+b-=10
+print(b)
+a*=10
+print(a)
+
