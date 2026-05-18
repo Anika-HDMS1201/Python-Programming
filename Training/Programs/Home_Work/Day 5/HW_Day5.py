9d2ed1f7f04e6cb9e235ccf3cc057f4e6218e29e
Anika-HDMS1201 <anikajana79@gmail.com>
2026-05-18 21:09:16 +0530

    Homework(half)

---
+++ Training/Programs/Home_Work/Day 5/HW_Day5.py
@@ -0,0 +1,39 @@
+#Task 1
+import random
+
+
+fruits=["Apple","Banana","Cherry"]
+a,b,c= fruits
+print(a,b,c)
+
+#Task 3
+x=int("150")
+print(x)
+
+#Task 4
+print("Hello",end="-")
+print("World")
+
+#Task 5
+a= 5+3j
+print(type(a))
+
+""" 
+Task 6
+"Python" word is assiged to 3 different variables a,b,c and then that is printed in one sigle line.
+"""
+
+#Task 7
+a=b=c="Python"
+print(a,b,c)
+
+#Task 8
+print(random.randrange(1,1000))
+
+#Task 9
+
+
+
+
+
+
