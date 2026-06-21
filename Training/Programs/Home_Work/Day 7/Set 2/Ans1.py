f724f0facadab9bfedb93e4d8d7afd38acfb3ca5
Anika-HDMS1201 <anikajana79@gmail.com>
2026-06-21 20:42:27 +0530

    HW of Day 8

---
+++ Training/Programs/Home_Work/Day 7/Set 2/Ans1.py
@@ -0,0 +1,8 @@
+total_bill = 1250
+friends = 3
+bill= total_bill + (total_bill* 10 /100) 
+eachpay= bill//3
+remaingchange= bill% 3
+print(bill)
+print(eachpay)
+print(remaingchange)
---
+++ Training/Programs/Home_Work/Day 7/Set 2/Ans2.py
@@ -0,0 +1,11 @@
+salary = 85000
+if salary < 50000:
+    withtax= (salary *(10/100))
+elif salary> 1000000:
+    withtax=(salary *(20/100))
+else:
+    withtax=(salary *(30/100))
+    print(withtax)
+    
+    
+
